31abc0011d7cda08001bd2e6ea50f8e842e6b29f
Ayan Baig <ayanbaig@iak.ngo>
2026-05-22 17:29:19 +0500

    Add caching for model training function
    Refactor model training into a cached function for efficiency.

--- app.py
+++ app.py
@@ -187,8 +187,15 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-model = XGBClassifier()
-model.fit(X_train, y_train)
+@st.cache_resource
+def train_model():
+
+    model = XGBClassifier()
+    model.fit(X_train, y_train)
+
+    return model
+
+model = train_model()
 
 # =========================
 # UI LAYOUT
